5c5e8f92c00e6746647b7e3e9f32b5a23066cefe
ClementTsang <clementjhtsang@gmail.com>
2020-02-21 01:07:38 -0500

    Update to 0.2.1

--- bottom.rb
+++ bottom.rb
@@ -1,7 +1,7 @@
 class Bottom < Formula
   desc "A feature-full and cross-platform graphical process/system monitor."
   homepage "https://github.com/ClementTsang/bottom"
-  url "https://github.com/ClementTsang/bottom/releases/download/0.2.0/bottom_x86_64-apple-darwin.tar.gz"
+  url "https://github.com/ClementTsang/bottom/releases/download/0.2.1/bottom_x86_64-apple-darwin.tar.gz"
   sha256 "3338ff86474345986746a11e042857b4903011e5bde99e20659f37d5992c196c"
   version "0.2.1"
 
