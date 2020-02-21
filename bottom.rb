5a0ccdd5af285da35d1e4d8703340ce9ee1db9ac
ClementTsang <clementjhtsang@gmail.com>
2020-02-21 01:04:41 -0500

    Update to 0.2.1

--- bottom.rb
+++ bottom.rb
@@ -2,12 +2,12 @@ class Bottom < Formula
   desc "A feature-full and cross-platform graphical process/system monitor."
   homepage "https://github.com/ClementTsang/bottom"
   url "https://github.com/ClementTsang/bottom/releases/download/0.2.0/bottom_x86_64-apple-darwin.tar.gz"
-  sha256 "a4f903fc9d80a2a483676663b4c3cb59bd02e072ab129d5a35e1bfd7333212aa"
-  version "0.2.0"
+  sha256 "3338ff86474345986746a11e042857b4903011e5bde99e20659f37d5992c196c"
+  version "0.2.1"
 
   def install
     bin.install "btm"
     ohai "You're done!  Run with \"btm\""
-    ohai "If you want to configure bottom, by default bottom looks for a btm.toml file in ~/.config/btm/"
+    ohai "If you want to configure bottom, by default bottom looks for a file in $HOME/.config/bottom/bottom.toml"
   end
 end
