c50409e3115206cffc75646877398ce3c18b3014
ClementTsang <clementjhtsang@gmail.com>
2020-02-25 19:29:34 -0500

    0.2.2

--- bottom.rb
+++ bottom.rb
@@ -1,9 +1,9 @@
 class Bottom < Formula
-  desc "A feature-full and cross-platform graphical process/system monitor."
+  desc "A cross-platform graphical process/system monitor with a customizable interface and a multitude of features."
   homepage "https://github.com/ClementTsang/bottom"
-  url "https://github.com/ClementTsang/bottom/releases/download/0.2.1/bottom_x86_64-apple-darwin.tar.gz"
-  sha256 "3338ff86474345986746a11e042857b4903011e5bde99e20659f37d5992c196c"
-  version "0.2.1"
+  url "https://github.com/ClementTsang/bottom/releases/download/0.2.2/bottom_x86_64-apple-darwin.tar.gz"
+  sha256 "e0ebb8f436c68168450f3b06ca4027b6bb3e3acbc4a2398d01f21a06d7b3a809"
+  version "0.2.2"
 
   def install
     bin.install "btm"
