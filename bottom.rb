60e6171f2c35c3112e0a5bdb4d5ed5d7fec2f2c9
ClementTsang <clementjhtsang@gmail.com>
2020-02-26 19:02:35 -0500

    Change sha.

--- bottom.rb
+++ bottom.rb
@@ -2,7 +2,7 @@ class Bottom < Formula
   desc "A cross-platform graphical process/system monitor with a customizable interface and a multitude of features."
   homepage "https://github.com/ClementTsang/bottom"
   url "https://github.com/ClementTsang/bottom/releases/download/0.2.2/bottom_x86_64-apple-darwin.tar.gz"
-  sha256 "e0ebb8f436c68168450f3b06ca4027b6bb3e3acbc4a2398d01f21a06d7b3a809"
+  sha256 "992b599ea472f9eaaeaa3c43f41f94557645a47ad77b948a703c4d040a346c0a"
   version "0.2.2"
 
   def install
