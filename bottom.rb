87772a4ec6c6cc6560d854cbc0e78e0cce9917b8
Ali Asad Lotia <ali.asad.lotia@gmail.com>
2020-07-06 08:04:37 +0100

    Use version var in download URl and bump version
    Previously although version was defined in the formula file, it wasn't
    being used in the download URL. This change uses the version variable in
    the download URL.

--- bottom.rb
+++ bottom.rb
@@ -1,9 +1,9 @@
 class Bottom < Formula
   desc "A cross-platform graphical process/system monitor with a customizable interface and a multitude of features."
   homepage "https://github.com/ClementTsang/bottom"
-  url "https://github.com/ClementTsang/bottom/releases/download/0.4.3/bottom_x86_64-apple-darwin.tar.gz"
-  sha256 "19f7d625fc173ac9178aba9098a057fb3254d72a880703c91bf0834f10045ad0"
   version "0.4.4"
+  url "https://github.com/ClementTsang/bottom/releases/download/#{version}/bottom_x86_64-apple-darwin.tar.gz"
+  sha256 "19f7d625fc173ac9178aba9098a057fb3254d72a880703c91bf0834f10045ad0"
 
   def install
     bin.install "btm"
