1c70cdfb10d7fd9093fa74005ebcc75ee9d883d6
ClementTsang <cjhtsang@uwaterloo.ca>
2020-07-08 17:53:27 -0400

    Proper 0.4.5

--- bottom.rb
+++ bottom.rb
@@ -1,8 +1,8 @@
 class Bottom < Formula
   desc "A cross-platform graphical process/system monitor with a customizable interface and a multitude of features."
   homepage "https://github.com/ClementTsang/bottom"
-  url "https://github.com/ClementTsang/bottom/releases/download/0.4.3/bottom_x86_64-apple-darwin.tar.gz"
-  sha256 "bf730fe20c5b9cd5dda09c2c61f5e7c2034e10e1721e182ecfc7e4bbd0962d35"
+  url "https://github.com/ClementTsang/bottom/releases/download/0.4.5/bottom_x86_64-apple-darwin.tar.gz"
+  sha256 "966dbd50954714bf2e904f882fe4e3da5c284095e5f89b1e4eba6aa7ed9e8c63"
   version "0.4.5"
 
   def install
