f20d11bc75c948288fcdb8e2692c0c020d0105c3
ClementTsang <ClementTsang@users.noreply.github.com>
2020-12-11 04:25:29 +0000

    Actions: Update homebrew package files to 0.5.4

--- bottom.rb
+++ bottom.rb
@@ -1,13 +1,13 @@
 class Bottom < Formula
     desc "A cross-platform graphical process/system monitor with a customizable interface and a multitude of features."
     homepage "https://github.com/ClementTsang/bottom"
-    version "0.5.3"
+    version "0.5.4"
     if OS.mac?
       url "https://github.com/ClementTsang/bottom/releases/download/#{version}/bottom_x86_64-apple-darwin.tar.gz"
-      sha256 "a426a96e73aa5190165e06897820f17ff84af830f868e27c0253b947c0d1f021"
+      sha256 "c98d7e56952a12367bafda50c3f4c5644781fcb3c1d73b3b216234a354d27a6b"
     elsif OS.linux?
-      url "https://github.com/ClementTsang/bottom/releases/download/#{version}/bottom_x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f434897d9bc0b5db8aea7a7fe4e2aab4790715d8871317c3e9912e4520cbbfa9"
+      url "https://github.com/ClementTsang/bottom/releases/download/#{version}/bottom_x86_64-unknown-linux-musl.tar.gz"
+      sha256 "94538d41973da791f57616fcdf75488b3b9c9f5e5bd156a6b1ac6c3b2ab8066b"
     end
   
     def install
